ac760ce00016b1b778ea31b8489acea3f47db6e4
MRLIGHT02 <saifali42020@gmail.com>
2025-06-09 12:33:56 +0530

    Add MVC setup and new models/controllers
    Updated Program.cs to register MVC services and enable routing. Removed default GET mapping. Created HomeController with an Index action method. Added Person model with properties for user data.

---
+++ ModelValidation/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace ModelValidation.Models
+{
+    public class Person
+    {
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public long? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public double? Price { get; set; }
+
+    }
+}
