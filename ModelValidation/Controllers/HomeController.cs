e1d164b391be9dc5a64ddb6af7820fcb6df5a28a
MRLIGHT02 <saifali42020@gmail.com>
2025-06-09 13:35:34 +0530

    #

--- ModelValidation/Controllers/HomeController.cs
+++ ModelValidation/Controllers/HomeController.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidation.Models;
 
 namespace ModelValidation.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        [Route("register")]
+        public IActionResult Index(Person person)
         {
-            return View();
+            return Content($"{person}");
         }
     }
 }
--- ModelValidation/Models/Person.cs
+++ ModelValidation/Models/Person.cs
@@ -1,13 +1,22 @@
-﻿namespace ModelValidation.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidation.Models
 {
     public class Person
     {
+        [Required]
         public string? PersonName { get; set; }
+        [Required]
         public string? Email { get; set; }
         public long? Phone { get; set; }
         public string? Password { get; set; }
         public string? ConfirmPassword { get; set; }
         public double? Price { get; set; }
 
+
+        public override string ToString()
+        {
+            return $"Name: {this.PersonName} Email: {this.Email} Phone: {this.Phone} Passowrd: {Password} Conform:{this.ConfirmPassword} Price: {this.Price}";
+        }
     }
 }
