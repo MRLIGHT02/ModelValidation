de70e73b6dcf195e163e8bab7b705a74573244dd
MRLIGHT02 <saifali42020@gmail.com>
2025-06-23 22:11:56 +0530

    Add MCustomValidation class for custom validation
    Introduced a new class `MCustomValidation` that extends
    `ValidationAttribute` in the `ModelValidation.CustomModelValidation`
    namespace. The class overrides the `IsValid` method, currently
    calling the base implementation without additional logic.

---
+++ ModelValidation/CustomModelValidation/MCustomValidation.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidation.CustomModelValidation
+{
+    public class MCustomValidation : ValidationAttribute
+    {
+        public override ValidationResult? IsValid(object? value, ValidationContext validationcontext)
+        {
+            return base.IsValid(value);
+        }
+    }
+}
